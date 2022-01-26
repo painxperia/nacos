7c227606db206236750d080c9076e17c44784f92
zpzhang <zpzhang@cisco.com>
2022-01-26 17:18:34 +0800

    提交

--- service/src/main/java/com/zpain/service/config/JsonConfig.java
+++ service/src/main/java/com/zpain/service/config/JsonConfig.java
@@ -3,6 +3,7 @@ package com.zpain.service.config;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
